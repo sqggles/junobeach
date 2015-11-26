003bd6b3ff887d31999d7150a935b980c67a8edd
Ajish George <ajish@rootedinsights.com>
2015-11-26 17:22:47 -0500

    add console consumer and refactor topic creator

--- scripts/create-kafka-topic.sh
+++ scripts/create-kafka-topic.sh
@@ -1,2 +1,4 @@
+KAFKA_HOME=/opt/local/kafka_2.11-0.9.0.0
+ZKHOST=localhost:2181
 TOPIC=testing
-./bin/kafka-topics.sh --zookeeper sandbox:2181 --create --topic $TOPIC --partitions 1 --replication-factor 1
+$KAFKA_HOME/bin/kafka-topics.sh --zookeeper $ZKHOST --create --topic $TOPIC --partitions 1 --replication-factor 1
