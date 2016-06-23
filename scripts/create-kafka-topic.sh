1e6d5de404e814fb83c85ab27cb06e9193378591
Ajish George <ajish@rootedinsights.com>
2016-06-23 11:53:20 -0500

    json output with optional print flag

--- scripts/create-kafka-topic.sh
+++ scripts/create-kafka-topic.sh
@@ -1,4 +1,3 @@
-KAFKA_HOME=/opt/local/kafka_2.11-0.9.0.0
 ZKHOST=localhost:2181
 TOPIC=testing
-$KAFKA_HOME/bin/kafka-topics.sh --zookeeper $ZKHOST --create --topic $TOPIC --partitions 1 --replication-factor 1
+kafka-topics --zookeeper $ZKHOST --create --topic $TOPIC --partitions 1 --replication-factor 1
--- stream-generator/generate_records.py
+++ stream-generator/generate_records.py
@@ -6,57 +6,54 @@ import time
 import random
 import argparse
 import logging
+import json
 
 LOG_FMT='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s'
 logger = logging.getLogger(__name__)
 
-class Address(dexml.Model):
-    street = fields.String(tagname="street")
-    city = fields.String(tagname="city")
-    state = fields.String(tagname="state")
-    zip = fields.String(tagname="zip")
+class Rx(dexml.Model):
+    npi = fields.Integer(tagname = "npi")
+    date_received = fields.Integer(tagname = "date_received")
+    svcbr_prescription_id = fields.Integer(tagname = "svcbr_prescription_id") 
+    drug_name = fields.String(tagname="drug_name")
+    patient_id = fields.Integer(tagname="patient_id")
+    prescription_id = fields.Integer(tagname="prescription_id")
+    status= fields.String(tagname="status_text")
+    svcbr_id = fields.Integer(tagname="svcbr_id")
 
+# prescription staging table
 
-class RandomXMLAddressProducer:
+fake_factory = Factory.create('en_US')
+class RandomRecordProducer:
     #universal fake factory for producer class
-    fake_factory = Factory.create('en_US')
-    @classmethod
-    def create_address(cls):
-        addr = cls.fake_factory.address()
-        (street, local) = addr.split("\n")
-        lsplits = local.split()
-        if len(lsplits) == 4:
-            # two words in city name
-            city = lsplits[0] + " " + lsplits[1]
-            (state, zip) = (lsplits[2:])
-        elif len(lsplits) == 3:
-            #one word city name
-            (city, state, zip) = lsplits
-            city = city.replace(",","")
-        address = Address(street=street, city=city, state=state, zip=zip)
-	# TODO: handle places with arbitrary number of words
-	# 3: West New York, NJ
-	# 4: El Paso De Robles, CA 
-	# 5: Friendly Village of Crooked Creek, GA
-	return address.render(fragment=True)
-    @classmethod
-    def produce(cls, producer, topic, min_delay=0, max_delay=50, max_messages=None):
+    def create_record(self):
+        npi = fake_factory.ean8()
+        date_received = fake_factory.unix_time()
+        rx_id = fake_factory.ean8()
+        drug_name = "Drug"
+        pt_id = fake_factory.ean8()
+        status = "TEST STATUS"
+        svc_id = fake_factory.ean8()
+        t = Rx(npi=npi, date_received=date_received, svcbr_prescription_id=rx_id, drug_name=drug_name, patient_id=pt_id, prescription_id=rx_id, status=status, svc_id=svc_id)
+        return t
+    def produce(self, producer, topic, min_delay=0, max_delay=50, max_messages=None, pr=False):
         ct = 0
         topic = bytes(topic)
-        addr = None
         while True:
-            if max_messages and ct == max_messages:
+            if max_messages and ct >= max_messages:
                 logger.debug("Shutting down")
                 break
             try:
-                addr = cls.create_address()
+                addr = self.create_record()
+                if pr:
+                    print json.dumps(addr.__dict__)
+                producer.send_messages(topic, bytes(addr.__dict__))
             except Exception as e:
                 logger.debug(e)
                 #not going to worry for fake data
                 continue
             if ct % 10000 == 0:
                 logger.info("Produced %d messages. Last record: %s" % (ct, addr))
-            producer.send_messages(topic, bytes(addr))
             #hackitude: sleep some milliseconds in range
             sleep_time = random.randrange(min_delay, max_delay)/1000.0
             logger.debug("Sleeping %.6f" % sleep_time)
@@ -67,11 +64,7 @@ class RandomXMLAddressProducer:
 
 def get_args():
     parser = argparse.ArgumentParser(
-        description="produce random US addresses in XML format and put them on\
-                     a kafka topic\n" +
-                    "ex: <Address><street>698 Arlin Crescent</street> \
-                     <city>Violetmouth</city><state>MO</state> \
-                     <zip>66549-5530</zip></Address>"
+        "generate records according to given cassandra schema"
     )
     # Add arguments
     parser.add_argument(
@@ -86,12 +79,14 @@ def get_args():
         '-l', '--limit', type=int, help='Maximum number of messages to send, def: Inf', required=False, default=None)
     parser.add_argument(
         '-v', '--verbose', help='Turn on debug logging', action="store_true")
+    parser.add_argument(
+        '-p', '--printed', help='show messages on stdout', action="store_true")
     args = parser.parse_args()
-    return args.kafka, args.topic, args.mindelay, args.maxdelay, args.limit, args.verbose
+    return args.kafka, args.topic, args.mindelay, args.maxdelay, args.limit, args.verbose, args.printed
 
 
 if __name__ == '__main__':
-    (kafka_host, topic, min_delay, max_delay, limit, verbose) = get_args()
+    (kafka_host, topic, min_delay, max_delay, limit, verbose, pr) = get_args()
     if verbose:
         logging.basicConfig(
             format=LOG_FMT,
@@ -105,5 +100,6 @@ if __name__ == '__main__':
 
     kafka = KafkaClient(kafka_host)
     producer = SimpleProducer(kafka)
-    RandomXMLAddressProducer.produce(producer, topic, min_delay, max_delay, limit)
+    rp = RandomRecordProducer()
+    rp.produce(producer, topic, min_delay, max_delay, limit, pr)
 
